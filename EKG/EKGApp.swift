e022571fbd7eeb25b7aafa224e7ced531011a1e6
Krzysztof Chodkiewicz <kchod.98@gmail.com>
2021-03-08 14:01:59 +0100

    Initial Commit

---
+++ EKG/EKGApp.swift
@@ -0,0 +1,20 @@
+//
+//  EKGApp.swift
+//  EKG
+//
+//  Created by Krzysztof Chodkiewicz on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct EKGApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
